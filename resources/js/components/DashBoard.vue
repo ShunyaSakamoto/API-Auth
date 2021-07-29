34c6fbb6e0ec82d0f57c71705944e9bb463dfd83
阪本　春矢 <sakamoto@ics21.jp>
2021-07-29 16:29:06 +0900

    見た目を調整

--- resources/js/components/DashBoard.vue
+++ resources/js/components/DashBoard.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
+                <div class="card mt-5">
                     <div class="card-header">Account Information</div>
 
                     <div class="card-body">
